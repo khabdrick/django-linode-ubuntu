af54a7db0be4fe44f9d0c0ea29f4d8f86dc292d8
Muhammed Ali <muhamzyali@gmail.com>
2022-08-08 19:18:32 +0100

    final commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    details = models.CharField(max_length=500, blank=False)
+    lead_owner = models.ForeignKey(
+        User, related_name="lead", on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
